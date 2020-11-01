404858c6fa6f41627539d5f7256684c7bc914880
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-11-01 17:28:31 +0600

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,8 @@
     <title>ciBlog</title>
     <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <script src="http://cdn.ckeditor.com/4.15.0/standard/ckeditor.js"></script>
   </head>
   <body>
